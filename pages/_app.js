b92595187f85c307d91daa3dadf9ba72ee79b3ab
korn <kornvik.t@hotmail.com>
2021-09-30 18:44:43 +0900

    all code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+// import {GetServerSideProps} from 'next';
+
+import Navbar from '../components/Navbar';
+import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
+// import { auth, firestore } from '../lib/firebase';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  
+  const userData = useUserData();
 
-export default MyApp
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
+}
+export default MyApp;
